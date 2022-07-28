abc61d2f80cf0e302069b970224a97130ae54763
DevJ1003 <devjoshi1384@gmail.com>
2022-07-29 02:08:18 +0530

    commit

---
+++ section10/interface_sort_struct.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+type person []string
+
+func (p person) Len() int {
+	return len(p)
+}
+
+func (p person) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p person) Less(i, j int) bool {
+	return p[i] < p[j] //Using > this will not work!
+}
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+func main() {
+
+	s := person{"Susan", "Tyler", "Ava", "Nick"}
+	fmt.Println(s)
+
+	sort.Sort(s)
+	fmt.Println(s)
+
+	sort.Sort(sort.Reverse(sort.StringSlice(s)))
+	fmt.Println(s)
+
+}
