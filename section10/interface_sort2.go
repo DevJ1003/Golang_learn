b10f2996ad5c98d5e470c9ba6b1c326580ade62d
DevJ1003 <devjoshi1384@gmail.com>
2022-07-29 01:34:26 +0530

    commit

---
+++ section10/interface_sort2.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	n := []int{7, 2, 10, 14, 1, 14, 9}
+	fmt.Println(n)
+
+	sort.Sort(sort.IntSlice(n))
+	fmt.Println(n)
+
+	sort.Sort(sort.Reverse(sort.IntSlice(n)))
+	fmt.Println(n)
+
+	// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+	s := []string{"Susan", "Tyler", "Ava", "Nick"}
+	fmt.Println(s)
+
+	sort.Sort(sort.StringSlice(s))
+	fmt.Println(s)
+
+	sort.Sort(sort.Reverse(sort.StringSlice(s)))
+	fmt.Println(s)
+
+}
